7c10df1ac48837511465cf2ce138ea8345767e5a
Sharaf <sharafa839@gmail.com>
2021-02-23 12:56:33 -0800

    Initial Commit

---
+++ ArabicGrammarWinner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArabicGrammarWinner
+//
+//  Created by ahmed on 23/02/2021.
+//  Copyright © 2021 ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
